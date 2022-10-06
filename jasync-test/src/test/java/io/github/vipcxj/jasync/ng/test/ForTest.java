204f2ff10d4e6b1c03535c6104103623fb05b9fc
vipcxj <vipcxj@126.com>
2022-10-06 16:35:42 +0800

    add a test for for-loop.

---
+++ jasync-test/src/test/java/io/github/vipcxj/jasync/ng/test/ForTest.java
@@ -0,0 +1,32 @@
+package io.github.vipcxj.jasync.ng.test;
+
+import io.github.vipcxj.jasync.ng.spec.JHandle;
+import io.github.vipcxj.jasync.ng.spec.JPromise;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ForTest {
+
+    /**
+     * it will loop forever, so just test compile.
+     * @return not important.
+     */
+    @SuppressWarnings({"StatementWithEmptyBody", "InfiniteLoopStatement", "unused"})
+    public JPromise<Void> emptyForLoop() {
+        for (;;) { }
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public JPromise<Void> interruptableForLoop() throws InterruptedException {
+        for (;;) {
+            JPromise.sleep(1, TimeUnit.SECONDS).await();
+        }
+    }
+
+    @Test
+    public void testInterruptableForLoop() throws InterruptedException {
+        JHandle<Void> handle = interruptableForLoop().async();
+        handle.cancel();
+    }
+}
